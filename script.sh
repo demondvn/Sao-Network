ec1dfa43164beb3c141cb508d20f6511dc191065
MonPham <demondhunter93@gmail.com>
2023-03-13 18:13:08 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-saod init NodeName --chain-id="sao-testnet0"
+saod init $NODE_NAME --chain-id="sao-testnet0"
 curl -Ls https://ss-t.sao.nodestake.top/genesis.json > $HOME/.sao/config/genesis.json 
 $(which saod) start
